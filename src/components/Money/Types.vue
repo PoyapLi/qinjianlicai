5f3c1d72e4c01c45c78ee93136a8f7cdd7205c46
Poyap <ryougiako@gmail.com>
2021-09-02 15:32:04 +0800

    使用 JS 实现了 Money 切换支出/收入组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected' " @click="selectType('-')">支出</li>
+      <li :class=" type === '+' && 'selected' " @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 export default {
-  name: 'Types'
+  name: 'Types',
+  props:['xxx'],
+  data(){
+    return {
+      type:'-' // '-'表示支出, '+'表示收入
+    }
+  },
+  methods:{
+    selectType(type){ // type 只能是 '-' 或 '+'
+      if(type !== '-' && type !== '+'){
+        throw new Error('type is unknow')
+      }
+      this.type = type
+    }
+  }
 };
 </script>
 
