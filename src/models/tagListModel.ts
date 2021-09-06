811e0842edbbe2a35ef410dee374f9470fe70fca
Poyap <ryougiako@gmail.com>
2021-09-06 14:56:26 +0800

    解决 id 生成问题

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagsList';
 
 type Tag = {
@@ -19,12 +21,13 @@ const tagListModel: TagListModel = {
         this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
         return this.data;
     },
-    create(name) {
+    create(name: string) {
         const names = this.data.map(item => item.name);
         if (names.indexOf(name) >= 0) {
             return 'duplicated';
         }
-        this.data.push({id: name, name: name});
+        const id = createId().toString();
+        this.data.push({id, name: name});
         this.save();
         return 'success';
     },
