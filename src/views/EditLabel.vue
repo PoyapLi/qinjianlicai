9168ca9d38f9aa0c4f1311849afe76a9f931d0e6
Poyap <ryougiako@gmail.com>
2021-09-06 14:34:30 +0800

    可以编辑和删除标签了

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,16 +1,18 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" field-name="标签名"
+      <FormItem :value="tag.name"
+                @update:value="update"
+                field-name="标签名"
                 placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
 
   </Layout>
@@ -27,7 +29,8 @@ import Button from '@/components/Button.vue';
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
-  tag?: {id: string, name: string} = undefined;
+  tag?: { id: string, name: string } = undefined;
+
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
@@ -38,6 +41,21 @@ export default class EditLabel extends Vue {
     } else {
       this.$router.replace('/404');
     }
+  }
+
+  update(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+
+  remove() {
+    if(this.tag){
+      tagListModel.remove(this.tag.id);
+    }
+  }
+  goBack(){
+    this.$router.back()
   }
 }
 </script>
@@ -73,11 +91,13 @@ export default class EditLabel extends Vue {
 
   }
 }
+
 .form-wrapper {
   margin-top: 8px;
   background: white;
 }
-.button-wrapper{
+
+.button-wrapper {
   text-align: center;
   padding: 16px;
   margin-top: 28px;
