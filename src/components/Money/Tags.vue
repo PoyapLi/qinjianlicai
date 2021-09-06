09dc579c0e3462aa401f3c4e4b7acdd098265da9
Poyap <ryougiako@gmail.com>
2021-09-06 22:05:13 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,16 +19,15 @@ import {Component} from 'vue-property-decorator';
 @Component({
   computed:{
     tagList(){
-      //TODO
-      // return this.$store.fetchTags();
-      return [];
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
-  tagList = store.fetchTags();
   selectedTags: string[] = [];
-
+  created(){
+    this.$store.commit('fetchTags')
+  }
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
@@ -44,8 +43,7 @@ export default class Tags extends Vue {
     if (!name) {
       return window.alert('标签名不能为空');
     }
-      //TODO
-      // store.createTag(name);
+    this.$store.commit('createTag',name);
     }
 }
 
