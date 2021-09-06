caf3f688dc3932bd7f4df4ed837e17efcc6fe390
Poyap <ryougiako@gmail.com>
2021-09-06 16:42:49 +0800

    封装 window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -16,20 +16,14 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
-tagListModel.fetch();
 
 @Component
 export default class Labels extends Vue{
-  tags = tagListModel.data;
+  tags = window.tagList;
   createTag(){
     const name = window.prompt('请输出标签名');
     if(name){
-      const message = tagListModel.create(name);
-      if(message === 'duplicated'){
-        window.alert('标签名重复')
-      }else if(message === 'success'){
-        window.alert('添加成功')
-      }
+      window.createTag(name);
     }
   }
 
