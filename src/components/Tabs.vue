a6e7ce33c84a9b7483d7ceb09637405ebf7175f5
Poyap <ryougiako@gmail.com>
2021-09-08 15:35:22 +0800

    抽离 Tabs.vue ，使用在 Statistics.vue 里

---
+++ src/components/Tabs.vue
@@ -0,0 +1,65 @@
+<template>
+  <ul class="tabs">
+    <li v-for="item in dataSource" :key="item.value"
+        :class="liClass(item)" @click="select(item)">{{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+type DataSourceItem = { text: string, value: string };
+
+@Component
+export default class Tabs extends Vue {
+  @Prop({required: true, type: Array})
+  dataSource!: DataSourceItem[];
+  @Prop(String)
+  readonly value!: string;
+  @Prop(String)
+  classPrefix?: string;
+
+  liClass(item: DataSourceItem) {
+    return {
+      [this.classPrefix + '-tabs-item']: this.classPrefix,
+      selected: item.value === this.value
+    };
+  }
+
+  select(item: DataSourceItem) {
+    this.$emit('update:value', item.value);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+.tabs {
+  display: flex;
+  background: #c4c4c4;
+  font-size: 24px;
+  text-align: center;
+
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: $color-highlight;
+    }
+  }
+}
+</style>
