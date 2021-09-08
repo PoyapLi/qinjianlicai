8bb9381e082ecc0a20731e8ba33d1a272c4e88f5
Poyap <ryougiako@gmail.com>
2021-09-08 21:24:17 +0800

    完成统计页面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,16 +1,15 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <ol>
-      <li v-for="(group,index) in result" :key="index">
-        <h3 class="title">{{ group.title }}</h3>
+      <li v-for="(group, index) in groupedList" :key="index">
+        <h3 class="title">{{ beautify(group.title) }}<span>¥{{group.total}}</span></h3>
         <ol>
           <li v-for="item in group.items" :key="item.id"
               class="record"
           >
-            <span>{{tagString(item.tags)}}</span>
-            <span class="notes">{{item.notes}}</span>
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
             <span>¥{{ item.amount }}</span>
           </li>
         </ol>
@@ -24,33 +23,63 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
-import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
+import dayjs from 'dayjs';
+import clone from '@/lib/clone';
+
 
 @Component({
   components: {Tabs},
 })
 
 export default class Statistics extends Vue {
-  tagString(tags: Tag[]){
-    return tags.length === 0 ? '无' : tags.join(',')
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
   }
+
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天';
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('MM月D日');
+    } else {
+      return day.format('YYYY年MM月D日');
+    }
+  }
+
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
 
-  get result() {
+  get groupedList() {
     const {recordList} = this;
-    type HashTableValue = { title: string, items: RecordList[] };
+    if (recordList.length === 0) {return [];}
 
-    const hashTable: { [key: string]: HashTableValue } = {};
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createdAt!.split('T');
-      hashTable[date] = hashTable[date] || {title: date, items: []};
+    const newList = clone(recordList)
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
 
-      hashTable[date].items.push(recordList[i]);
+    type Result = {title: string, total?:number, items: RecordItem[]}[]
+    const result: Result = [{title: dayjs(recordList[0].createdAt).format('YYYY-MM-DD'),items: [newList[0]]}];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+      }
     }
-    return hashTable;
+    result.map(group=>{
+      group.total = group.items.reduce((sum, item)=> sum + item.amount,0)
+    })
+    return result;
   }
 
   beforeCreate() {
@@ -58,8 +87,6 @@ export default class Statistics extends Vue {
   }
 
   type = '-';
-  interval = 'day';
-  intervalList = intervalList;
   recordTypeList = recordTypeList;
 }
 </script>
@@ -67,7 +94,7 @@ export default class Statistics extends Vue {
 <style scoped lang="scss">
 @import '~@/assets/style/helper.scss';
 
-%item{
+%item {
   padding: 8px 16px;
   line-height: 24px;
   display: flex;
@@ -75,15 +102,17 @@ export default class Statistics extends Vue {
   align-content: center;
 }
 
-.title{
+.title {
   @extend %item;
 }
-.record{
+
+.record {
   background: white;
   @extend %item;
 }
-.notes{
-  margin-right:auto;
+
+.notes {
+  margin-right: auto;
   margin-left: 16px;
   color: #999;
 }
@@ -91,7 +120,7 @@ export default class Statistics extends Vue {
 
 ::v-deep {
   .type-tabs-item {
-    background: #c4c4c4;
+    background: rgb(245, 245, 245);
 
     &.selected {
       background: $color-highlight;
